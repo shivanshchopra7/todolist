6c988b75d20f138d721811c9e95a8adae7f55859
Shivansh Chopra <gauravshivansh7@gmail.com>
2024-01-12 03:32:07 +0530

    updating functionality

--- app/page.js
+++ app/page.js
@@ -4,18 +4,44 @@ import React, { useState } from 'react'
 const page = () => {
   const [title, settitle] = useState("")
   const [desc, setdesc] = useState("")
+  const [mainTask, setmainTask] = useState([])
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log(title)
-    console.log(desc)
+   setmainTask([...mainTask, {title,desc}]);
     setdesc("")
     settitle("")
+    console.log(mainTask)
   }
+const deleteHandler = (i) => {
+let copytask = [...mainTask]
+copytask.splice(i,1)
+setmainTask(copytask)
+}
+
+  let renderTask = <h2 className='font-bold'>No task available</h2>
+if(mainTask.length>0){
+ renderTask = mainTask.map((t,i)=> {
+return( 
+<li key={i} className='flex items-center justify-between mb-5'>
+<div className='flex justify-between mb-5 w-2/3'>
+  <h5 className='text-2xl font-semibold'>{t.title}</h5>
+  <h6 className='text-lg font-medium'>{t.desc}</h6>
+</div>
+<button 
+onClick={() => {
+  deleteHandler(i)
+}}
+className='bg-red-400 text-white px-4 py-2
+rounded font-bold'>Delete</button>
+</li>
+)
+  })
+}
   return (
     <>
-   <h1 className='bg-black text-white p-5 text-4xl font-bold text-center'>My To Do List</h1>
+   <h1 className='bg-black text-white p-5 text-4xl font-bold text-center '>My To Do List</h1>
 <form onSubmit={submitHandler}>
-  <input type="text" className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2' placeholder='Enter task here' value={title} onChange={(e)=>{
+  <input type="text" className='text-2xl  border-zinc-800 border-4 m-8 px-4 py-2' placeholder='Enter task here' value={title} onChange={(e)=>{
     settitle(e.target.value)
   } }/> 
    
@@ -26,7 +52,12 @@ const page = () => {
   }/>  
 <button className='bg-black text-white px-4 py-3 m-5 text-2xl font-bold rounded'>Add Task</button>
 </form>
-
+<hr />
+<div className='p-8 bg-slate-200 '>
+<ul>
+  {renderTask}
+</ul>
+</div>
    </>
   )
 }
